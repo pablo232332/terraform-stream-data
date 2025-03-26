76bb293a7677f89fd290ad012a32de9b00b340ab
Pawell <69118424+pablo232332@users.noreply.github.com>
2025-03-26 01:44:08 +0100

    Update provider.tf

--- setups/provider.tf
+++ setups/provider.tf
@@ -9,4 +9,10 @@ terraform {
       version = ">= 5.0.0"
     }
   }
+ backend "s3" {
+    bucket = "terraform-data-stream-bucket"
+    key = "prod/app/backend.tfstate"
+    region ="eu-central-1"
+    encrypt = true
+  }
 }
